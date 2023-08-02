be7e13ae05ec7235736ab63c86ecae453f0ccc4f
reeshkov <a.ryzhkov@rosalinux.ru>
2023-08-02 10:43:46 +0300

    test w reply

--- notifycation/src/main.cpp
+++ notifycation/src/main.cpp
@@ -1,30 +1,61 @@
 #include <QCoreApplication>
+#include <QStringList>
 #include <QDebug>
-#include <QObject>
 #include <QTimer>
 #include <KNotification>
+#include <KNotificationReplyAction>
+#include <memory>
+
+class Notificator : public QObject
+{
+    Q_OBJECT
+
+public:
+    Notificator(QObject * parent = nullptr) : QObject(parent) {}
+
+private slots:
+    void doNotify();
+
+signals:
+    void finished();
+};
+
+void Notificator::doNotify()
+{
+    QString filePath = QStringLiteral("/home/omv/picture.png");
+    KNotification *notification = new KNotification("captured");
+    notification->setComponentName(QStringLiteral("plasma_phone_components"));
+    notification->setTitle(QStringLiteral("Nice picture"));
+    notification->setUrls({QUrl::fromLocalFile(filePath)});
+    notification->setText(QString("Nice picture is in <br/><b>%1</b>").arg(filePath));
+    notification->setIconName(filePath);
+
+    //QStringList actions{QStringLiteral("OK")};
+    //notification->setActions(actions);
+
+    auto replyAction = std::unique_ptr<KNotificationReplyAction>(new KNotificationReplyAction(QStringLiteral("Reply")));
+    replyAction->setPlaceholderText(QStringLiteral("Reply to Dave..."));
+    QObject::connect(replyAction.get(), &KNotificationReplyAction::replied, [](const QString &text)
+                     {
+                         qDebug() << "you replied with" << text;
+                     });
+    notification->setReplyAction(std::move(replyAction));
+
+
+    connect(notification, &KNotification::closed, this, &Notificator::finished);
+
+    notification->sendEvent();
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    KNotification *notification = new KNotification("test");
-    notification->setComponentName(QStringLiteral("notifycation"));
-    notification->setTitle(QStringLiteral("Title"));
-    notification->setText(QString("Text"));
+    Notificator n;
 
-    QObject::connect(notification, &KNotification::closed, &app, [&app, notification](){
-        qDebug() << "closed" << notification->appName();
-        app.quit();
-    });
-    QObject::connect(notification, static_cast<void (KNotification::*)(unsigned int)>(&KNotification::activated), &app, [](unsigned int action){ qDebug() << "activated" << action; }, Qt::QueuedConnection);
-    QObject::connect(notification, &KNotification::ignored, &app, [](){ qDebug() << "ignored"; });
-    QObject::connect(notification, &KNotification::defaultActivated, &app, [](){ qDebug() << "defaultActivated"; });
+    QObject::connect(&n, SIGNAL(finished()), &app, SLOT(quit()));
 
-
-    QTimer::singleShot(1, notification, SLOT(sendEvent()));
-    
-    qDebug() << "started";
+    QTimer::singleShot(0, &n, SLOT(doNotify()));
 
     return app.exec();
 }
