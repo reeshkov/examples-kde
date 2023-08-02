e031770223f04502c32f035a6634f74c96661b4b
reeshkov <a.ryzhkov@rosalinux.ru>
2023-08-02 10:19:34 +0300

    test notify

---
+++ notifycation/src/main.cpp
@@ -0,0 +1,30 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QObject>
+#include <QTimer>
+#include <KNotification>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    KNotification *notification = new KNotification("test");
+    notification->setComponentName(QStringLiteral("notifycation"));
+    notification->setTitle(QStringLiteral("Title"));
+    notification->setText(QString("Text"));
+
+    QObject::connect(notification, &KNotification::closed, &app, [&app, notification](){
+        qDebug() << "closed" << notification->appName();
+        app.quit();
+    });
+    QObject::connect(notification, static_cast<void (KNotification::*)(unsigned int)>(&KNotification::activated), &app, [](unsigned int action){ qDebug() << "activated" << action; }, Qt::QueuedConnection);
+    QObject::connect(notification, &KNotification::ignored, &app, [](){ qDebug() << "ignored"; });
+    QObject::connect(notification, &KNotification::defaultActivated, &app, [](){ qDebug() << "defaultActivated"; });
+
+
+    QTimer::singleShot(0, notification, SLOT(sendEvent()));
+
+    return app.exec();
+}
+
+#include "main.moc"
