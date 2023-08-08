ce0467666f702a52156d82762fb7b9be57bbf5a4
reeshkov <a.ryzhkov@rosalinux.ru>
2023-08-08 15:47:47 +0300

    add jobtype notifi

--- notifycation/src/main.cpp
+++ notifycation/src/main.cpp
@@ -5,10 +5,50 @@
 #include <KNotificationReplyAction>
 //#include <memory>
 
+#include <KJob>
+#include <KUiServerV2JobTracker>
+
+class TestJob : public KJob
+{
+    Q_OBJECT
+
+public:
+    TestJob(QObject *parent = nullptr);
+    void start() override;
+public slots:
+    void finish();
+};
+
+
+TestJob::TestJob(QObject *parent)
+    : KJob(parent)
+{
+    setProcessedAmount(KJob::Items, 1);
+    setTotalAmount(KJob::Items, 1);
+    auto caps = capabilities();
+    caps.setFlag(KJob::Suspendable, true);
+    caps.setFlag(KJob::Killable, true);
+    setCapabilities(caps);
+}
+void TestJob::start()
+{
+    qDebug() << "KJob::start";
+
+    Q_EMIT description(this,QStringLiteral("Moving"),
+                       qMakePair(QStringLiteral("Source"),QUrl(QStringLiteral("/asourcefile")).toString())
+                       , qMakePair(QStringLiteral("Destination"),QUrl(QStringLiteral("trash:/")).toString()));
+    QTimer::singleShot(1000, this, &TestJob::finish);
+}
+void TestJob::finish()
+{
+    qDebug() << "KJob::finish";
+    emitResult();
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
-    qDebug() << "started";
+    qDebug() << "test started";
 
     QStringList args(QCoreApplication::arguments());
     int args_count = args.count();
@@ -17,6 +57,8 @@ int main(int argc, char *argv[])
     if(2 <= args_count) event = args.at(1);
     QString appName("notifytest");
     if(3 <= args_count) appName = args.at(2);
+    app.setApplicationName(appName);
+
     QString title("Title");
     if(4 <= args_count) title = args.at(3);
     QString text("Text");
@@ -24,9 +66,10 @@ int main(int argc, char *argv[])
 
     KNotification *notification = new KNotification(event);
     notification->setComponentName(appName);
+    notification->setIconName("system-file-manager");
     notification->setTitle(title);
     notification->setText(text);
-    notification->setFlags(KNotification::Persistent);
+    notification->setFlags(KNotification::Persistent | KNotification::SkipGrouping);
     notification->setUrls({QUrl(QStringLiteral("trash:/"))});
 
 
@@ -43,17 +86,38 @@ int main(int argc, char *argv[])
 //    notification->setReplyAction(std::move(replyAction));
 
     QObject::connect(notification, &KNotification::closed, &app, [&app, notification](){
-        qDebug() << "closed" << notification->appName() << notification->id();
-        app.quit();
+        qDebug() << "notify closed" << notification->appName() << notification->id();
+        //app.quit();
     });
     QObject::connect(notification, static_cast<void (KNotification::*)(unsigned int)>(&KNotification::activated), &app, [](unsigned int action){ qDebug() << "activated" << action; }, Qt::QueuedConnection);
     QObject::connect(notification, &KNotification::ignored, &app, [](){ qDebug() << "ignored"; });
     QObject::connect(notification, &KNotification::defaultActivated, &app, [](){ qDebug() << "defaultActivated"; });
 
 
-    QTimer::singleShot(1, notification, SLOT(sendEvent()));
+    QTimer::singleShot(10, notification, SLOT(sendEvent()));
 
-    qDebug() << "exec";
+    TestJob * m_job = new TestJob(nullptr);
+    //m_job->setProperty("destUrl", QUrl(QStringLiteral("trash:/")));
+    m_job->setFinishedNotificationHidden(false);
+
+
+    QObject::connect(m_job, &KJob::finished, &app, [] {
+        qDebug() << "KJob::finished";
+    });
+    QObject::connect(m_job, &KJob::suspended, &app, [] {
+        qDebug() << "KJob::suspended";
+    });
+    QObject::connect(m_job, &KJob::result, &app, [] {
+        qDebug() << "KJob::result";
+    });
+    KUiServerV2JobTracker * m_uiServerV2Tracker = new KUiServerV2JobTracker(nullptr);
+    m_job->setProperty("desktopFileName",appName);
+    m_uiServerV2Tracker->registerJob(m_job);
+
+    QTimer::singleShot(0, m_job, &TestJob::start);
+
+
+    qDebug() << "exec: notificationId=" << notification->id() << appName;
     return app.exec();
 }
 
